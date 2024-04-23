3df8325604a11eca7c563ed92e8344b99ad896a6
QINGYUN RAN <cyancloudran@gmail.com>
2024-04-23 15:07:41 +0100

    Add func for calculate span.

---
+++ func/find_spans.py
@@ -0,0 +1,19 @@
+def find_span(target, document):
+    idx = 0
+    spans = []
+    span = []
+
+    for i, token in enumerate(document):
+        if token != target[idx]:
+            idx = 0
+            span = []
+            continue
+        span.append(i)
+        idx += 1
+        if idx == len(target):
+            spans.append(span)
+            span = []
+            idx = 0
+            continue
+
+    return spans
