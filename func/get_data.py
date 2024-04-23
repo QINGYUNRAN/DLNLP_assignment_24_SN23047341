505cb0a801dabda966f598d87cda02fe1bd499af
QINGYUN RAN <cyancloudran@gmail.com>
2024-04-23 15:53:49 +0100

    Update the fuctions for getting dataset.

--- func/get_data.py
+++ func/get_data.py
@@ -2,67 +2,39 @@ import os
 import json
 from itertools import chain
 import pandas as pd
+from datasets import Dataset
+from transformers import TrainingArguments, AutoTokenizer, AutoModelForTokenClassification, DataCollatorForTokenClassification
+from sklearn.model_selection import train_test_split
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(dir_path, "Dataset")
 train_data_path = os.path.join(data_path, 'train.json')
 test_data_path = os.path.join(data_path, "test.json")
 
-def get_data(train_data_path, test_data_path, fold):
+def get_data(train_data_path, test_data_path):
     data = json.load(open(train_data_path))
-    test_data = json.load(open(test_data_path))
-    all_labels = sorted(list(set(chain(*[x["labels"] for x in data]))))
+    # test_data = json.load(open(test_data_path))
+    all_labels = [
+        'B-EMAIL', 'B-ID_NUM', 'B-NAME_STUDENT', 'B-PHONE_NUM', 'B-STREET_ADDRESS', 'B-URL_PERSONAL', 'B-USERNAME',
+        'I-ID_NUM', 'I-NAME_STUDENT', 'I-PHONE_NUM', 'I-STREET_ADDRESS', 'I-URL_PERSONAL', 'O'
+    ]
     label2id = {l: i for i,l in enumerate(all_labels)}
     id2label = {v:k for k,v in label2id.items()}
-    # Create a DataFrame for the training data using the loaded training data.
-    df_train = pd.DataFrame(data)
+    target = [l for l in all_labels if l != "O"]
+    train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
 
-    # Add a new column 'fold' to the training DataFrame, representing the fold number.
-    df_train['fold'] = df_train['document'] % fold
+    train_ds = Dataset.from_dict({
+        "full_text": [x["full_text"] for x in train_data],
+        "document": [str(x["document"]) for x in train_data],
+        "tokens": [x["tokens"] for x in train_data],
+        "trailing_whitespace": [x["trailing_whitespace"] for x in train_data],
+        "provided_labels": [x["labels"] for x in train_data],
+    })
+    test_ds = Dataset.from_dict({
+        "full_text": [x["full_text"] for x in test_data],
+        "document": [str(x["document"]) for x in test_data],
+        "tokens": [x["tokens"] for x in test_data],
+        "trailing_whitespace": [x["trailing_whitespace"] for x in test_data],
+    })
 
-    # Create a DataFrame for the test data using the loaded test data.
-    df_test = pd.DataFrame(test_data)
-
-def find_span(target, document):
-    idx = 0
-    spans = []
-    span = []
-
-    # Iterate through the document tokens
-    for i, token in enumerate(document):
-        # If the current token doesn't match the target start anew
-        if token != target[idx]:
-            idx = 0
-            span = []
-            continue
-        # If token matches, append its index to the span list
-        span.append(i)
-        idx += 1
-        # If the entire target is found, append the span to the list of spans
-        if idx == len(target):
-            spans.append(span)
-            # Reset span and idx for next potential match
-            span = []
-            idx = 0
-            continue
-    return spans
-
-
-def downsample_df(train_df, percent):
-    # Add a new column 'is_labels' to indicate if labels are present in the sample
-    train_df['is_labels'] = train_df['labels'].apply(lambda labels: any(label != 'O' for label in labels))
-
-    # Separate samples with labels and samples without labels
-    true_samples = train_df[train_df['is_labels'] == True]
-    false_samples = train_df[train_df['is_labels'] == False]
-
-    # Calculate the number of false samples to keep after downsampling
-    n_false_samples = int(len(false_samples) * percent)
-
-    # Randomly sample false samples to downsample
-    downsampled_false_samples = false_samples.sample(n=n_false_samples, random_state=42)
-
-    # Concatenate true samples and downsampled false samples to create the downsampled DataFrame
-    downsampled_df = pd.concat([true_samples, downsampled_false_samples])
-
-    return downsampled_df
+    return train_ds, test_ds, label2id, id2label, target
